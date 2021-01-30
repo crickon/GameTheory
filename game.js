4565b1d9bd19208bfce436d0c6d0c0f090141aa8
mathewacarter29 <mathewacarter29@vt.edu>
2021-01-30 17:22:38 -0500

    update game.js

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
 class Player {
   constructor(mark) {
     this.mark = mark;
+    this.moves = [];
+    this.movesCount = 0;
   }
-  placeMark(x, y) {
-    board.board[x][y] = this.mark;
+
+}
+
+class RandomBot extends Player {
+  constructor(mark) {
+    super(mark);
   }
+
 }
 
 class Board {
@@ -14,7 +21,13 @@ class Board {
                   ['.','.','.']];
     this.player1 = player1;
     this.player2 = player2;
+    this.legalMoves = [];
   }
+
+  placeMark(player, x, y) {
+    this.board[x][y] = player.mark;
+  }
+
   printBoard() {
     var result = '';
     for (var row = 0; row < this.board.length; row++) {
@@ -25,13 +38,30 @@ class Board {
     }
     return result;
   }
+
+  getLegalMoves() {
+    for (var row = 0; row < this.board.length; row++) {
+      for (var col = 0; col < this.board[row].length; col++) {
+        if (this.board[row][col] == '.') {
+          this.legalMoves.push([row,col]);
+        }
+      }
+    }
+    return this.legalMoves;
+
+  }
   checkForWinner() {
-    var flag = false;
+    var player1Win = player1.mark.repeat(3);
+    var player2Win = player2.mark.repeat(3);
+    var winner = 'None'
     //check rows
     for (var row = 0; row < this.board.length; row++) {
       var arr = this.board[row].join('');
-      if (arr == 'xxx' || arr == 'ooo') {
-        flag = true;
+      if (arr == player1Win) {
+        winner = 'Player 1';
+      }
+      else if (arr == player2Win) {
+        winner = 'Player 2';
       }
     }
     //check columns
@@ -40,20 +70,29 @@ class Board {
       for (var row = 0; row < this.board.length; row++) {
         result += this.board[row][col];
       }
-      if (result == 'xxx' || result == 'ooo') {
-        flag = true;
+      if (result == player1Win) {
+        winner = 'Player 1';
+      }
+      else if (result == player2Win) {
+        winner = 'Player 2';
       }
     }
     //check diagonals
     var diag1 = this.board[0][0] + this.board[1][1] + this.board[2][2];
     var diag2 = this.board[2][0] + this.board[1][1] + this.board[0][2];
-    if (diag1 == 'xxx' || diag1 == 'ooo') {
-      return true;
+    if (diag1 == player1Win) {
+      winner = 'Player 1';
     }
-    if (diag2 == 'xxx' || diag2 == 'ooo') {
-      return true;
+    else if (diag1 == player2Win) {
+      winner = 'Player 2';
     }
-    return flag;
+    if (diag2 == player1Win) {
+      winner = 'Player 1';
+    }
+    else if (diag2 == player2Win) {
+      winner = 'Player 2';
+    }
+    return winner;
   }
 }
 
@@ -61,10 +100,12 @@ var player1 = new Player('x');
 var player2 = new Player('o');
 
 var board = new Board(player1, player2);
+board.placeMark(player1,0,0);
+board.placeMark(player2,1,2);
+board.placeMark(player1,2,2);
+board.placeMark(player1,1,1);
 
 
-player1.placeMark(0, 0);
-player1.placeMark(1, 1);
-player1.placeMark(2, 2);
 console.log(board.printBoard());
+console.log(board.getLegalMoves());
 console.log(board.checkForWinner());
