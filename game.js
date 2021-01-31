c6ee67a088af2e2a5951151fea6c07847f95be38
mathewacarter29 <mathewacarter29@vt.edu>
2021-01-31 00:06:17 -0500

    Finished random bots

--- game.js
+++ game.js
@@ -1,17 +1,7 @@
 class Player {
   constructor(mark) {
     this.mark = mark;
-    this.moves = [];
-    this.movesCount = 0;
   }
-
-}
-
-class RandomBot extends Player {
-  constructor(mark) {
-    super(mark);
-  }
-
 }
 
 class Board {
@@ -22,10 +12,12 @@ class Board {
     this.player1 = player1;
     this.player2 = player2;
     this.legalMoves = [];
+    this.getLegalMoves();
   }
 
   placeMark(player, x, y) {
     this.board[x][y] = player.mark;
+    this.getLegalMoves();
   }
 
   printBoard() {
@@ -40,6 +32,7 @@ class Board {
   }
 
   getLegalMoves() {
+    this.legalMoves = [];
     for (var row = 0; row < this.board.length; row++) {
       for (var col = 0; col < this.board[row].length; col++) {
         if (this.board[row][col] == '.') {
@@ -48,22 +41,23 @@ class Board {
       }
     }
     return this.legalMoves;
-
   }
+
   checkForWinner() {
-    var player1Win = player1.mark.repeat(3);
-    var player2Win = player2.mark.repeat(3);
-    var winner = 'None'
+    var player1Win = this.player1.mark.repeat(3);
+    var player2Win = this.player2.mark.repeat(3);
+
     //check rows
     for (var row = 0; row < this.board.length; row++) {
       var arr = this.board[row].join('');
       if (arr == player1Win) {
-        winner = 'Player 1';
+        return 'Player 1';
       }
       else if (arr == player2Win) {
-        winner = 'Player 2';
+        return 'Player 2';
       }
     }
+
     //check columns
     var result = '';
     for (var col = 0; col < this.board[0].length; col++) {
@@ -71,41 +65,85 @@ class Board {
         result += this.board[row][col];
       }
       if (result == player1Win) {
-        winner = 'Player 1';
+        return 'Player 1';
       }
       else if (result == player2Win) {
-        winner = 'Player 2';
+        return 'Player 2';
       }
+      result = '';
     }
+
     //check diagonals
     var diag1 = this.board[0][0] + this.board[1][1] + this.board[2][2];
     var diag2 = this.board[2][0] + this.board[1][1] + this.board[0][2];
     if (diag1 == player1Win) {
-      winner = 'Player 1';
+      return 'Player 1';
     }
     else if (diag1 == player2Win) {
-      winner = 'Player 2';
+      return 'Player 2';
     }
     if (diag2 == player1Win) {
-      winner = 'Player 1';
+      return 'Player 1';
     }
     else if (diag2 == player2Win) {
-      winner = 'Player 2';
+      return 'Player 2';
     }
-    return winner;
+    return 'Tie';
   }
 }
 
-var player1 = new Player('x');
-var player2 = new Player('o');
+class RandomBot {
+  constructor(player) {
+    this.player = player;
+  }
+  selectMove(board) {
+    var limit = board.legalMoves.length;
+    var move = Math.floor(Math.random() * limit);
+    return board.legalMoves[move];
+  }
+}
 
-var board = new Board(player1, player2);
-board.placeMark(player1,0,0);
-board.placeMark(player2,1,2);
-board.placeMark(player1,2,2);
-board.placeMark(player1,1,1);
+class oneLayerBot {
+  constructor(player, board) {
+
+  }
+}
+
+function createGame() {
+  var player1 = new Player('x');
+  var player2 = new Player('o');
+  var board = new Board(player1, player2);
 
 
-console.log(board.printBoard());
-console.log(board.getLegalMoves());
-console.log(board.checkForWinner());
+  var bot1 = new RandomBot(player1);
+  var bot2 = new RandomBot(player2);
+
+  console.log(board.printBoard());
+  var count = 0;
+  var bot;
+  while (board.legalMoves.length > 0 && board.checkForWinner() == 'Tie') {
+    if (count % 2 == 0) {
+      bot = bot1;
+    }
+    else {
+      bot = bot2;
+    }
+    var move = bot.selectMove(board);
+    console.log(move);
+    board.placeMark(bot.player, move[0], move[1])
+    console.log(board.printBoard());
+    count++;
+  }
+  console.log(board.checkForWinner());
+}
+
+createGame();
+
+// var player1 = new Player('x');
+// var player2 = new Player('o');
+// var board = new Board(player1, player2);
+//
+// board.placeMark(player1, 0,1);
+// board.placeMark(player1, 1,1);
+// board.placeMark(player1, 2,1);
+// console.log(board.checkForWinner());
